a47239717599b50fbfd6da48cd591756e22ea19a
LucasGabrielSS <143851170+LucasGabrielSS@users.noreply.github.com>
2023-10-26 20:39:58 -0300

    :sparkles: feat: add function to delete user

--- script.js
+++ script.js
@@ -78,9 +78,28 @@ function updateUser() {
 		});
 }
 
-// Exemplo de requisição delete para deletar o usuário com ID 1.
-fetch('https://dummyjson.com/users/1', {
-	method: 'DELETE',
-})
-	.then((res) => res.json())
-	.then(console.log);
+
+	function deleteUsers(){
+
+		fetch('https://dummyjson.com/users/1', {
+			method: 'DELETE',
+		})
+			.then((res) => {
+				console.log(`Status Code: ${res.status}`);
+				if (!res.ok) {
+					return console.log(`HTTP error! Status Code: ${res.status}`);
+				} else {
+					return res.json();
+				}
+			})
+			.then(() => {
+				alert('User successfully deleted')
+			})
+			.catch((error) => {
+				console.error(`Erro: ${error}`);
+				alert("Failed to delete the user");
+			})
+
+	}
+
+
